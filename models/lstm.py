f200cfb267b4d97555b1c1d344c16c4bc57cc18f
Muhammad Fahad <fahadfaadi829@gmail.com>
2026-08-05 13:24:40 +0500

    Add CNN BiLSTM sentiment model

--- models/lstm.py
+++ models/lstm.py
@@ -1,18 +1,23 @@
 import torch
 import torch.nn as nn
-# Bidirectional LSTM sentiment classifier
+# CNN BiLSTM sentiment classifier
 class SentimentLSTM(nn.Module):
     def __init__(self,vocab_size,embedding_dim=100,hidden_dim=128,dropout=0.3,embedding_weights=None):
         super().__init__()
         self.embedding=nn.Embedding(vocab_size,embedding_dim,padding_idx=0)
         if embedding_weights is not None:
             self.embedding.weight.data.copy_(embedding_weights)
-        self.lstm=nn.LSTM(embedding_dim,hidden_dim,batch_first=True,bidirectional=True)
+        self.conv=nn.Conv1d(embedding_dim,128,kernel_size=3,padding=1)
+        self.relu=nn.ReLU()
+        self.lstm=nn.LSTM(128,hidden_dim,batch_first=True,bidirectional=True)
         self.dropout=nn.Dropout(dropout)
         self.fc=nn.Linear(hidden_dim*2,1)
     def forward(self,x):
         embedded=self.embedding(x)
-        _,(hidden,_)=self.lstm(embedded)
+        embedded=embedded.transpose(1,2)
+        features=self.relu(self.conv(embedded))
+        features=features.transpose(1,2)
+        _,(hidden,_)=self.lstm(features)
         output=torch.cat((hidden[-2],hidden[-1]),dim=1)
         output=self.dropout(output)
         return self.fc(output).squeeze(1)
